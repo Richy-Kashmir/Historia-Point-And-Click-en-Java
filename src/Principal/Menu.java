f08c953df5eedd4b2a5544db8bcb46fb0737a075
richard <smashrichy@gmail.com>
2026-03-06 00:10:33 +0100

    Version1 vacía por conflictos de Mergeo Y JavaFX

--- src/Principal/Menu.java
+++
@@ -1,9 +0,0 @@
-package Principal;
-
-public class Menu {
-	public static void main(String[] args) {
-		ControlPrincipal.iniciar(); // Llama al método iniciar para comenzar el programa
-		ControlPrincipal.ejecutar(); // Llama al método ejecutar para iniciar el bucle principal del programa
-		
-	}
-}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module PointClick {
-	requires java.desktop;
-}
