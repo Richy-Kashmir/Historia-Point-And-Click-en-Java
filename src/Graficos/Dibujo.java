aaabd2cf8cca367df45a4d981fd950edb13d4851
fernando <hernandezjaramillo1998@gmail.com>
2026-03-09 20:54:58 +0100

    Se agregan imagen de juego y borrar botones

--- src/Graficos/Dibujo.java
+++ src/Graficos/Dibujo.java
@@ -23,6 +23,7 @@ import herramientas.Sonido;
 import herramientas.TransformadorImagenes;
 
 
+
 public class Dibujo extends Canvas {
 
     private static final long serialVersionUID = 1L;
@@ -48,6 +49,7 @@ public class Dibujo extends Canvas {
 	private JFrame ventana;
 	private boolean enPantallaOpciones = false;
 	private BufferedImage imagenLupa;
+	private boolean enPantallaDelJuego = false; // Para controlar si estamos en la pantalla del juego o no, y así evitar dibujar el menú o los botones invisibles
     
 
     public Dibujo(int ancho, int alto, long tiempoInicio, JFrame ventana) {
@@ -198,7 +200,8 @@ public class Dibujo extends Canvas {
 
     
   // ------------------------------------------------------------------------------------------ //  
-   
+    
+    
     // Métodos 
     
     
@@ -207,8 +210,10 @@ public class Dibujo extends Canvas {
     	
 	}
     
+    
     // ------------------------------------------------------------------------------------------ //
-     
+    
+    
     public void cambiarAImagenSecundaria() {
         imagenActual = imagenSecundaria;
         cambioRealizado = true; 
@@ -220,15 +225,29 @@ public class Dibujo extends Canvas {
         }
     }
     
+    
     // ------------------------------------------------------------------------------------------ //
     
     public void cambiarAImagenOficina() {
-		imagenActual = CargadorRecursos.cargarImagen("recursos/imagenes/Oficina.JPEG");
+    	int oficinaX = 270, oficinaY = 170, oficinaAncho = 260, oficinaAlto = 65;	
+    	boolean hoverOficina = false;
+    	
+    	imagenActual = CargadorRecursos.cargarImagen("recursos/imagenes/Oficina .JPEG");
 		cambioRealizado = true;  // Para mantener el flujo del programa y permitir hover (clics en los botones invisibles)
+		enPantallaDelJuego = true; // Ahora estamos en la pantalla del juego, así que no dibujamos el menú ni los botones invisibles
 		detenerMusica();  // Por si acaso
 		musicaFondo = new Sonido("recursos/musica/Corium.wav"); 
 		if (musicaFondo != null) {
 			musicaFondo.reproducir(true);  // Loop infinito
+		
+			Graphics2D g2d = (Graphics2D) graficos; // Para efectos de transparencia y suavizado
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // para redondear los bordes	
+			 if (hoverOficina) {
+	                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f)); // 25% de transparencia para el efecto de brillo
+	                g2d.setColor(new Color(255, 200, 50)); // Color para el menu hover (dorado)
+	                g2d.fillRoundRect(oficinaX, oficinaY, oficinaAncho, oficinaAlto, 15, 15); // El redondeado de bordes
+	                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)); // Restaurar opacidad completa para no afectar otros elementos
+	            }
 		}
 	} 
     
@@ -254,13 +273,16 @@ public class Dibujo extends Canvas {
         }
     }
    
+    
     // ------------------------------------------------------------------------------------------ //
     
     public void dibujar() {
     	if (!isDisplayable()) // Verifica si el canvas está listo para dibujar
     		return;
+    	
+    	try { // try-catch para evitar errores de dibujo si el canvas no está completamente inicializado
         buffer = getBufferStrategy(); // el buffer es lo que se va a mostrar en pantalla y se obtiene del canvas
-        if (buffer == null) {
+        if (buffer == null) { // Si el buffer no existe, lo creamos
             createBufferStrategy(3);
             return;
         }
@@ -280,16 +302,16 @@ public class Dibujo extends Canvas {
        
         // Si el cambio a la imagen secundaria ya se ha realizado, dibuja los botones invisibles y el efecto hover
    
-        if (cambioRealizado) {
+        if (cambioRealizado && !enPantallaDelJuego) { // Solo dibujamos el menú y los botones invisibles si ya se ha cambiado a la imagen secundaria y no estamos en la pantalla del juego
             Graphics2D g2d = (Graphics2D) graficos; // Para efectos de transparencia y suavizado
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // para redondear los bordes 
 
             // Brillo dorado al hacer hover sobre cada botón
             if (hoverJugar) {
-                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f));
-                g2d.setColor(new Color(255, 200, 50));
-                g2d.fillRoundRect(jugarX, jugarY, jugarAncho, jugarAlto, 15, 15);
-                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f)); // 25% de transparencia para el efecto de brillo
+                g2d.setColor(new Color(255, 200, 50)); // Color para el menu hover (dorado)
+                g2d.fillRoundRect(jugarX, jugarY, jugarAncho, jugarAlto, 15, 15); // El redondeado de bordes
+                g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)); // Restaurar opacidad completa para no afectar otros elementos
             }
             if (hoverOpciones) {
                 g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f));
@@ -312,5 +334,10 @@ public class Dibujo extends Canvas {
         graficos.drawImage(imagenLupa, (int)p.getX()-32, (int)p.getY()-32, 64, 64, null); // Dibuja la imagen de la lupa centrada en la posición del ratón
         graficos.dispose();
         buffer.show();
+        
+    	 } catch (Exception e) {
+    	        return ;
+    	     
     }
+}
 }
